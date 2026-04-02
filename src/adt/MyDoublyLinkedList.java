f26845fa45759641a77d8c859c91464a97e1389c
Taeyoung Hwang <taeyoungtaeold@gmail.com>
2026-04-02 17:34:42 -0400

    Saving progress for better Doubly LinkedList

--- src/adt/MyDoublyLinkedList.java
+++ src/adt/MyDoublyLinkedList.java
@@ -9,81 +9,76 @@ public class MyDoublyLinkedList {
         this.size = 0;
         head = new Node(-1); // To indicate head
         tail = new Node(-2); // To indicate tail
+        head.next = tail;
+        tail.prev = head;
     }
 
     public void addHead(Node node){
         if(size == 0){
             head.next = node;
-            tail = head.next;
-            size++;
+            node.prev = head;
+            node.next = tail;
+            tail.prev = node;
+            ++size;
         }
         else{
-            node.next = head.next;
+            Node temp = head.next;
             head.next = node;
+            node.next = temp;
+            temp.prev = node;
+            node.prev = head;
             ++size;
         }
     }
 
     public void deleteHead(){
         if(size == 0){
-            System.out.println("Invalid operation, No element to delete(Can't delete head)");
+            System.out.println("Invalid Operation: No elements to delete");
         }
         else if(size == 1){
-            head.next = null;
-            tail = head;
+            head.next = tail;
+            tail.prev = head;
             --size;
         }
         else{
-            head.next = head.next.next;
+            Node temp = head.next.next;
+            temp.prev = head;
+            head.next = temp;
             --size;
         }
     }
 
     public void addTail(Node node){
         if(size == 0){
-            head.next = node;
-            tail = head.next;
-            ++size;
+            addHead(node);
         }
-        else{
-            tail.next = node;
-            tail = node;
-            ++size;
-        }
+        Node temp = tail.prev;
+        temp.next = node;
+        node.prev = temp;
+        node.next = tail;
+        tail.prev = node;
+        ++size;
     }
 
     public void deleteTail(){
         if(size == 0){
-            System.out.println("Invalid Operation, there are no elements (Can't delete tail)");
+            System.out.println("No items to delete");
         }
-        else{
-            tail = tail.prev;
-            tail.next = null;
+        else if(size == 1){
+            head.next = tail;
+            tail.prev = head;
             --size;
         }
-    }
-
-    public void addIndex(int index, Node node){
-
-    }
-
-    public void deleteIndex(int index){
-
-    }
-
-    public void showAllValue(){
-        if(size == 0){
-            System.out.println("No elements to display");
-        }
         else{
-            Node temp = head.next;
-            while(temp != null){
-                System.out.print(temp.value + " ");
-                temp = temp.next;
-            }
-            System.out.println("- - - - - - ");
+
         }
     }
+
+    public void addIndex(int index, Node node){}
+
+    public void deleteIndex(int index){}
+
+    public void showAllValue(){}
 
 
 
