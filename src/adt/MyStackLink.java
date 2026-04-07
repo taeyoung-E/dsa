0d45b577eabad7d0f85fdc415cbfe05b41ecc73d
Taeyoung Hwang <taeyoungtaeold@gmail.com>
2026-04-07 15:01:57 -0400

    Incomplete implementation of MyStackLink

--- src/adt/MyStackLink.java
+++ src/adt/MyStackLink.java
@@ -1,4 +1,41 @@
 package adt;
 
+/*
+LinkedList implementation of
+ */
+
 public class MyStackLink {
+    Node sentinel; //Dummy head
+    Node tail; // // Pointer to reference the last node added in MyStackLink
+    int size; // Since there are no capacity, a counter to track the number of nodes in the list
+
+    public MyStackLink(){
+        sentinel = new Node(-1);
+        tail = sentinel;
+        size = 0;
+    }
+
+
+
+    public void push(){
+
+    }
+
+    public Node pop(){
+        return tail;
+    }
+
+    public Node peek(){
+        return tail;
+    }
+
+
+    static class Node{
+        Node next;
+        int value;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
 }
