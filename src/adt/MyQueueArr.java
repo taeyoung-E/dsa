8390ac2b8245433de380f29539030c72791ed8f8
Taeyoung Hwang <taeyoungtaeold@gmail.com>
2026-04-07 19:39:18 -0400

    Refactoring queue classes

--- src/adt/MyQueueArr.java
+++
@@ -1,4 +0,0 @@
-package adt;
-
-public class MyQueueArr {
-}
---
+++ src/adt/MyQueueArrLinear.java
@@ -0,0 +1,12 @@
+package adt;
+
+/*
+Enqueue from the rear and dequeue from the front
+
+ */
+
+public class MyQueueArrLinear {
+    public int[] capacity;
+    public int size;
+
+}
