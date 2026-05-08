689f87d64cc43f054097f37a3040eb89158bb498
Taeyoung Hwang <taeyoungtaeold@gmail.com>
2026-05-07 20:51:26 -0400

    Implemented Binary Search and Bubble, try out Binary search with duplicate

--- src/sort/MyBinary.java
+++ src/sort/MyBinary.java
@@ -1,4 +1,84 @@
 package sort;
 
+/*
+Two implementations:
+
+One that returns the first found value,and one that returns number of duplicates
+
+Let's try both iterative and recursive
+Though it seems like Recursive verison is faster, turns out it's because of JIT compiler warmup noise
+ */
+
 public class MyBinary {
+    static void main() {
+        int[] testArr = new int[1_000_000];
+
+        for(int i = 0; i < 1_000_000; i++){
+            testArr[i] = i;
+        }
+        int[] dupArr = {1,2,3,3,4,5,6,8,8,9};
+
+        iterBinarySearch(testArr,3000000);
+        System.out.println();
+        reverseBinarySearch(testArr,3000000);
+    }
+
+
+    public static void iterBinarySearch(int[] arr,int target){
+        long start = System.nanoTime();
+        int mid = arr.length / 2;
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low <= high){
+            if(arr[mid] == target){
+                System.out.println("Target found, at index: " + mid);
+                long end = System.nanoTime();
+                System.out.println(end-start);
+                return;
+            }
+            else if(target > arr[mid]){
+                low = mid + 1 ;
+                mid = (low + high) / 2;
+            }
+            else if(target < arr[mid]){
+                high = mid - 1;
+                mid = (low + high) / 2;
+            }
+        }
+        System.out.println("Target not found");
+        long end = System.nanoTime();
+        System.out.println("Time taken: " + (end - start));
+        return;
+    }
+
+    public static void reverseBinarySearch(int[] arr,int target){
+        long start = System.nanoTime();
+        int low = 0;
+        int high = arr.length - 1;
+        recurBinarySearch(arr,low,high,target);
+        long end = System.nanoTime();
+        System.out.println("Time taken: " + (end - start));
+    }
+
+    private static void recurBinarySearch(int[] arr,int low,int high,int target){
+        int mid = (low + high) / 2;
+        if(low > high){
+            System.out.println("Target not found");
+            return;
+        }
+        else if(arr[mid] == target){
+            System.out.println("Target found, at index: " + mid);
+            return;
+        }
+        else if(target > arr[mid]){
+            low = mid + 1;
+            recurBinarySearch(arr,low,high,target);
+        }
+        else if(target < arr[mid]){
+            high = mid - 1;
+            recurBinarySearch(arr,low,high,target);
+        }
+    }
+
 }
--- src/sort/MyBubble.java
+++ src/sort/MyBubble.java
@@ -1,4 +1,36 @@
 package sort;
 
+/*
+n + n-1 + n-2 + n-2 . . . + 1
+Number of comparison is sum of natural number formula therefore n(n-1) / 2
+ */
+
+import java.util.Arrays;
+
 public class MyBubble {
+    static void main() { // On duplicate, dont swap and continue
+        int[] testArr = {3,4,1,5,7,9,0,2,3};
+        System.out.println(Arrays.toString(testArr));
+        System.out.println();
+        System.out.println("After sort");
+        bubbleSort(testArr);
+        System.out.println(Arrays.toString(testArr));
+    }
+
+    public static void bubbleSort(int[] arr){
+        for(int i = 0; i < arr.length; i++){ // Testing out descending order
+            for(int j = 0; j < arr.length - 1 - i; j++){
+                if(arr[j] < arr[j+1]){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr,int index1,int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
 }
