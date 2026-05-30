9c26438a2046750b3f53135186ff587a511bfac4
Taeyoung Hwang <taeyoungtaeold@gmail.com>
2026-05-29 20:46:32 -0400

    Compressed String,if count of a character is 1,don't add the number, 2 methods. First one uses 2 pointer and the other one sentinel string

---
+++ src/problems/strings/question11/Main.java
@@ -0,0 +1,68 @@
+package problems.strings.question11;
+
+import java.util.Scanner;
+
+public class Main {
+    public String solution2(String input){
+        String sentinel = input + '\0';
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int count = 1;
+        for(i = 0; i < sentinel.length() - 1; i++){
+            if(sentinel.charAt(i) == sentinel.charAt(i+1)){
+                count++;
+            }
+            else{
+                if(count == 1){
+                    sb.append(sentinel.charAt(i));
+                }
+                else{
+                    sb.append(sentinel.charAt(i));
+                    sb.append(count);
+                    count = 1;
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public String solution(String input){
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        char[] container = input.toCharArray();
+        int left = 0, right = left;
+
+        while(right < container.length){
+            if(container[right] == container[left]){
+                count++;
+                right++;
+            }
+            else if(container[right] != container[left]){
+                if(count == 1){
+                    sb.append(container[left]);
+                }
+                else{
+                    sb.append(container[left]);
+                    sb.append(count);
+                }
+                count = 0;
+                left = right;
+            }
+        }
+        if(count == 1){
+            sb.append(container[left]);
+        }
+        else{
+            sb.append(container[left]);
+            sb.append(count);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Main t = new Main();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(t.solution2(scanner.nextLine()));
+    }
+}
