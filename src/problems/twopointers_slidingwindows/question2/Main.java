c43504bdc5c7468b66aec293b511b7210ad8cd21
Taeyoung Hwang <taeyoungtaeold@gmail.com>
2026-07-02 12:46:51 -0400

    Optimized solution for finding duplicate, sort first and then compare using left and right pointer

--- src/problems/twopointers_slidingwindows/question2/Main.java
+++ src/problems/twopointers_slidingwindows/question2/Main.java
@@ -4,14 +4,23 @@ import java.util.*;
 
 public class Main {
     public void solution(int[] input1, int[] input2){
+        Arrays.sort(input1);
+        Arrays.sort(input2);
         List<Integer> solution = new ArrayList<>();
-        for(int i = 0; i < input1.length; i++)
-            for(int j = 0; j < input2.length; j++){
-                if(input1[i] == input2[j]){
-                    solution.add(input1[i]);
-                }
+        int left = 0, right = 0;
+        while(left < input1.length && right < input2.length){
+            if(input1[left] == input2[right]){
+                solution.add(input1[left]);
+                left++;
+                right++;
             }
-        solution.sort(null);
+            else if(input1[left] < input2[right]){
+                left++;
+            }
+            else{
+                right++;
+            }
+        }
         for(int i : solution){
             if(i == solution.get(solution.size() - 1)){
                 System.out.print(i);
@@ -23,8 +32,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Main t = new Main();
         Scanner sc = new Scanner(System.in);
+        Main t = new Main();
         int n = sc.nextInt();
         int[] input = new int[n];
         for(int i = 0; i < input.length; i++){
@@ -37,5 +46,45 @@ public class Main {
         }
 
         t.solution(input,input2);
+
     }
+
+
+
+
+//    public void solution(int[] input1, int[] input2){ //Correct but slow
+//        List<Integer> solution = new ArrayList<>();
+//        for(int i = 0; i < input1.length; i++)
+//            for(int j = 0; j < input2.length; j++){
+//                if(input1[i] == input2[j]){
+//                    solution.add(input1[i]);
+//                }
+//            }
+//        solution.sort(null);
+//        for(int i : solution){
+//            if(i == solution.get(solution.size() - 1)){
+//                System.out.print(i);
+//            }
+//            else{
+//                System.out.print(i + " ");
+//            }
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Main t = new Main();
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int[] input = new int[n];
+//        for(int i = 0; i < input.length; i++){
+//            input[i] = sc.nextInt();
+//        }
+//
+//        int[] input2 = new int[sc.nextInt()];
+//        for(int i = 0; i < input2.length; i++){
+//            input2[i] = sc.nextInt();
+//        }
+//
+//        t.solution(input,input2);
+//    }
 }
